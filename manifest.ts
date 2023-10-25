105c5dde5139ac3d3ed9de29919db4c5d69474c2
Filip Maj <fmaj@slack-corp.com>
2023-10-25 16:50:58 -0400

    updates

--- manifest.ts
+++ manifest.ts
@@ -1,5 +1,6 @@
 import { Manifest, DefineEvent, Schema, DefineType } from "deno-slack-sdk/mod.ts";
 import ApprovalWorkflow from "./workflows/approval.ts";
+import RemoveWorkflow from "./workflows/remove-user.ts";
 
 const fancyAssBoolean = DefineType({
   name: "fancyAssBoolean",
@@ -19,8 +20,8 @@ export default Manifest({
   name: "interactive-disapproval",
   description: "Approving allthethings",
   icon: "icon.png",
-  workflows: [ApprovalWorkflow],
+  workflows: [ApprovalWorkflow, RemoveWorkflow],
   outgoingDomains: ["raw.githubusercontent.com"],
-  botScopes: ["commands", "chat:write", "chat:write.public", "metadata.message:read", "channels:history"],
+  botScopes: ["commands", "chat:write", "chat:write.public", "metadata.message:read", "channels:history", "files:read", "usergroups:write"],
   events: [MyEvent],
 });
